6f9da0ebfa1a5ce28c4137135b95f7f941af830d
kumarshyam7777 <kumar.shyam7777@gmail.com>
2019-11-26 16:01:36 +0530

    Initial commit

---
+++ ifelseelif.py
@@ -0,0 +1,9 @@
+brand=input("Enter the Beer/Whiskey Brand:")
+if brand=="RC":
+    print("Your Whiskey Brand is Royal Challenge :"),
+elif brand=="KF":
+    print("Your Beer Brand is KingFisher"),
+elif brand=="BP":
+    print("Your Whiskey Brand is Blender Pride"),
+else:
+    print("The brand is not availabe here")
---
+++ test.py
@@ -0,0 +1,10 @@
+n1=int(input("enter first number"))
+n2=int(input("enter second number"))
+n3=int(input("Enter third number"))
+if n1>n2 and n1>n3:
+    print("Biggest Number is",n1),
+elif  n2>n3:
+
+        print("Biggest Number is",n2),
+else:
+            print("Biggest Number is",n3)
